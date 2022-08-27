522fe118b76e38e477c7da09a61c1f7dc4776e49
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2022-08-27 09:57:44 -0600

    Fin de templates/plantillas

--- users/register/urls.py
+++ users/register/urls.py
@@ -1,6 +1,8 @@
-from django.urls import URLPattern, path
+from django.urls import path
 from . import views
 
 urlpatterns = [ # Conexión con urls de la carpeta principal del proyecto.
-    path('', views.index, name="index") #Se recibe de views.py y se "carga" a urls.py de la carpeta del proyecto.
+    path('', views.index, name="index"), #Se recibe de views.py y se "carga" a urls.py de la carpeta del proyecto.
+    path('cursopy/', views.cursopy, name="cursopy"),
+    path('cursocss/', views.cursocss, name="cursocss"),
 ]
